d9c77fa93e68572f086b9bd90efc2248400f6f38
Dawid Gałęziewski <Dawid.Galeziewski@empik.com>
2020-06-24 12:50:55 +0200

    update: created conditional css for input

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,33 @@
-// Styles
-import './css/globalStyles.css';
-
 // Libs
-import React, {useState, useEffect} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import axios from 'axios';
-import {Global, css} from '@emotion/core';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import axios from "axios";
+import { Global, css, jsx } from "@emotion/core";
 
 // Components
-import MainContainer from './components/MainContainer/MainContainer';
-import Tank from './components/Pages/Tank';
-import Login from './components/Pages/Login';
-import TanksHome from './components/Pages/TanksHome';
-import UserProfile from './components/Pages/UserProfile';
-import About from './components/Pages/About';
+import MainContainer from "./components/MainContainer/MainContainer";
+import Tank from "./components/Pages/Tank";
+import Login from "./components/Pages/Login";
+import TanksHome from "./components/Pages/TanksHome";
+import UserProfile from "./components/Pages/UserProfile";
+import About from "./components/Pages/About";
 // Utils
-import { getUserToken, formatAuthorizationHeader, formatBearerToken} from './utils/tokenUtils';
-import {pipe} from './utils/generalUtils';
+import {
+  getUserToken,
+  formatAuthorizationHeader,
+  formatBearerToken,
+} from "./utils/tokenUtils";
+import { pipe } from "./utils/generalUtils";
+
+/** @jsx jsx */
+const globalStyles = css`
+  html {
+    font-size: 10px;
+  }
+  body {
+    font-size: 16px;
+  }
+`;
 
 function App() {
   const [tokenState, setTokenState] = useState(null);
@@ -24,41 +35,77 @@ function App() {
   const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
 
   const loginOnMount = async (isUserLoggedIn, tokenState) => {
-
     const token = getUserToken(tokenState);
-    if(!isUserLoggedIn && token){
-      const authHeader = pipe( formatBearerToken, formatAuthorizationHeader)(token);
-      try{
-        const {data} = await axios.get('/users/me', {headers: authHeader});
+    if (!isUserLoggedIn && token) {
+      const authHeader = pipe(
+        formatBearerToken,
+        formatAuthorizationHeader
+      )(token);
+      try {
+        const { data } = await axios.get("/users/me", { headers: authHeader });
         setTokenState(token);
         setIsUserLoggedIn(true);
-        setUserAccountInfo(data)
-      } catch(error){
-        console.log(error)
+        setUserAccountInfo(data);
+      } catch (error) {
+        console.log(error);
       }
     }
-  }
+  };
 
-  useEffect( ()=> {
-    loginOnMount(isUserLoggedIn, tokenState)
-  }, [isUserLoggedIn])
+  useEffect(() => {
+    loginOnMount(isUserLoggedIn, tokenState);
+  }, [isUserLoggedIn]);
 
-
-  // const globalStyles= css`
-  //   background-color: #fefefe;
-  // `;
   return (
     <div className="App">
-          <Router>
-            <MainContainer setIsUserLoggedIn={setIsUserLoggedIn} setTokenState={setTokenState}>
-              <Route path="/" exact render={() => <Login isUserLoggedIn={isUserLoggedIn} setIsUserLoggedIn={setIsUserLoggedIn} tokenState={tokenState} setTokenState={setTokenState}/>} 
+      <Global styles={globalStyles} />
+      <Router>
+        <MainContainer
+          setIsUserLoggedIn={setIsUserLoggedIn}
+          setTokenState={setTokenState}
+        >
+          <Route
+            path="/"
+            exact
+            render={() => (
+              <Login
+                isUserLoggedIn={isUserLoggedIn}
+                setIsUserLoggedIn={setIsUserLoggedIn}
+                tokenState={tokenState}
+                setTokenState={setTokenState}
               />
-              <Route path="/tanks" exact render={() => <TanksHome isUserLoggedIn={isUserLoggedIn} tokenState={tokenState} />} />
-              <Route path="/tanks/:tankID" exact component={Tank} tokenState={tokenState}/>
-              <Route path="/me" exec render={()=> <UserProfile userAccountInfo={userAccountInfo}  isUserLoggedIn={isUserLoggedIn} setIsUserLoggedIn={setIsUserLoggedIn} />} />
-              <Route path="/about" exec component={About} />
-            </MainContainer>
-          </Router>
+            )}
+          />
+          <Route
+            path="/tanks"
+            exact
+            render={() => (
+              <TanksHome
+                isUserLoggedIn={isUserLoggedIn}
+                tokenState={tokenState}
+              />
+            )}
+          />
+          <Route
+            path="/tanks/:tankID"
+            exact
+            component={Tank}
+            tokenState={tokenState}
+          />
+          <Route
+            path="/me"
+            exec
+            render={() => (
+              <UserProfile
+                userAccountInfo={userAccountInfo}
+                isUserLoggedIn={isUserLoggedIn}
+                setIsUserLoggedIn={setIsUserLoggedIn}
+              />
+            )}
+          />
+          <Route path="/about" exec component={About} />
+        </MainContainer>
+      </Router>
     </div>
   );
 }
--- client/src/components/atoms/Inputs/InputSlider.js
+++ client/src/components/atoms/Inputs/InputSlider.js
@@ -1,41 +1,62 @@
-import React from 'react';
-import {css, jsx} from '@emotion/core';
+import React, { useState } from "react";
+import { css, jsx } from "@emotion/core";
 /** @jsx jsx */
 
 const inputWrapperStyle = css`
-    position: relative;
-    display: inline-block;
-    margin-bottom: 1rem;
+  position: relative;
+  display: inline-block;
+  margin-bottom: 1rem;
 `;
 
 const inputStyle = css`
-    padding: 0.8rem;
-    border: none;
-    border-bottom: 1px solid #92a2ae;
-    outline: none;
+  padding: 0.8rem;
+  border: none;
+  border-bottom: 1px solid #92a2ae;
+  outline: none;
+  &:focus {
+    border-bottom: 1px solid #5a95e6;
+  }
 `;
 
-const labelStyle = css`
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    font-size: 1.6rem;
-    transform: translate(-50%, -50%);
-    color: #92a2ae;
-    input:focus + & {
-        transform: translate(-50%, -150%) scale(0.8);
-    }
-    transition: transform ease-in 0.3s;
+const labelStyle = ({ isValueFilled }) => css`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  font-size: 1.6rem;
+  transform: ${isValueFilled
+    ? "translate(-50%, -150%) scale(0.8)"
+    : "translate(-50%, -50%)"};
+  color: #92a2ae;
+  input:focus + & {
+    transform: translate(-50%, -150%) scale(0.8);
+    color: #5a95e6;
+  }
+  transition: all ease-in 0.3s;
 `;
 
 const InputSlider = () => {
+  const [isValueFilled, setIsValueFilled] = useState(false);
 
-    return (
-        <div css={inputWrapperStyle}>
-            <input css={inputStyle} id="test" type="text" name="test" />
-            <label css={labelStyle} htmlFor="test">Test input</label>
-        </div>
-    )
-}
+  const onChangeHandler = (event) => {
+    setIsValueFilled(event.target.value.length > 0);
+    console.log(isValueFilled);
+  };
+
+  return (
+    <div css={inputWrapperStyle}>
+      <input
+        onChange={onChangeHandler}
+        css={inputStyle}
+        id="test"
+        type="text"
+        name="test"
+        min="1"
+      />
+      <label css={labelStyle({ isValueFilled })} htmlFor="test">
+        Test input
+      </label>
+    </div>
+  );
+};
 
 export default InputSlider;
